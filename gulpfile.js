3a60e8e602f5324370439d9caf5d55c92ec0875b
Matt McFadyen <mmcfadyen91@gmail.com>
2014-09-24 22:42:34 -0400

    basic gulpfile fully functional :pizza::cat:

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,37 @@ var gulp    = require('gulp')
    ,stylus  = require('gulp-stylus')
    ,jade 	= require('gulp-jade')
    ,jshint	= require('gulp-jshint')
-   ,watch	= require('gulp-watch');
-
-gulp.task('test', function(){
-	console.log('hello world');
-});
+   ,watch	= require('gulp-watch')
+   ,gutil   = require('gulp-util');
 
 gulp.task('stylus', function(){
+	gutil.log(gutil.colors.cyan('starting Stylus'));
 	gulp.src('./app/assets/css/main.styl')
 	.pipe(stylus())
 	.pipe(gulp.dest('./public'));
 });
 
-gulp.task('default',['test']);
+gulp.task('jade', function(){
+	gutil.log(gutil.colors.cyan('starting Jade'));
+	gulp.src('./app/assets/templates/*.jade')
+	.pipe(jade({pretty: true}))
+	.pipe(gulp.dest('./public'));
+})
+
+gulp.task('jshint', function(){
+	gutil.log(gutil.colors.cyan('starting JShint'));
+	gulp.src('./app/assets/javascript/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default', { verbose: true }));
+})
+
+gulp.task('watch', function(){
+	gulp.watch('./app/assets/css/*.styl', ['stylus']);
+	gulp.watch('./app/assets/javascript/*.js', ['jshint']);
+	gulp.watch('./app/assets/templates/**/*.jade',['jade']);
+
+})
+
+gulp.task('build',['stylus','jade']);
+
+gulp.task('default',['build','jshint','watch']);
